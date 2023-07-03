86a9744cece813e06b17eebaaac1db3cf355ac01
sshahniaz <100251uk@saeinstitute.edu>
2023-07-03 12:30:03 +0100

    Added the activities pages

--- app/src/main/java/com/example/personnel/Messages.java
+++ app/src/main/java/com/example/personnel/Messages.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-//    This is the login page / main page
+public class Messages extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_messages);
     }
 }
