85a651a1638d60a28ae0072f9f9747335ac0b6c0
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2023-07-08 10:10:32 +0100

    created top card for shift information and clock in/out button

--- app/src/main/java/com/example/personnel/DashBoard.java
+++ app/src/main/java/com/example/personnel/DashBoard.java
@@ -10,5 +10,6 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
     }
 }
