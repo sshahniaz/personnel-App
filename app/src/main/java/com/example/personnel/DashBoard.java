8c84119e4c7fd881fde841ffa9476455fb38736a
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2023-07-11 12:09:35 +0100

    create insert method and test

--- app/src/main/java/com/example/personnel/DashBoard.java
+++ app/src/main/java/com/example/personnel/DashBoard.java
@@ -2,7 +2,10 @@ package com.example.personnel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class DashBoard extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+//        create object from db helper class
+        DBHelper objDBHelper = new DBHelper(this);
+
+//        call writable method to insert values to database
+        SQLiteDatabase db = objDBHelper.getWritableDatabase();
+
+//        create values object for key value pairs to pass to writable method
+        ContentValues values = new ContentValues();
+
+//      test values to insert into database
+        values.put(objDBHelper.date, "11/07/2023");
+        values.put(objDBHelper.clockInTime, "09:00");
+        values.put(objDBHelper.clockOutTime, "17:00");
+
+//        insert to database
+        long result = db.insert("attendance", null, values);
+
+//        feedback on status
+        if(result ==-1) {
+            Toast.makeText(DashBoard.this, "Values not inserted", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(DashBoard.this, "Values inserted on row" + result, Toast.LENGTH_LONG).show();
+        }
     }
 }
