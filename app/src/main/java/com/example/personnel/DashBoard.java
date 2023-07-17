4754f005b9facc82c4156f653a1d0df16870c819
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2023-07-17 09:00:10 +0100

    update dashboard class

--- app/src/main/java/com/example/personnel/DashBoard.java
+++ app/src/main/java/com/example/personnel/DashBoard.java
@@ -32,64 +32,24 @@ public class DashBoard extends AppCompatActivity {
         setContentView(R.layout.activity_dash_board);
 
 
-//      *******  CLOCK IN AND CLOCK OUT FUNCTION *******
-
+//      *******  DEFAULT FRAGMENT DISPLAY *******
 
 
 //      set variables for clock in/out function
         datetime = (TextView) findViewById(R.id.dateTime);
         datetime2 = (TextView) findViewById(R.id.dateTime2);
-//        clockBtn = (Button) findViewById(R.id.clockBtn);
         shiftStart = (TextView) findViewById(R.id.shiftStart);
         shiftEnd = (TextView) findViewById(R.id.shiftEnd);
         displayBtn=(FrameLayout) findViewById(R.id.buttonFragment);
 
 
-//        create fragment object
+//        create fragment object and set as default fragment
 
         clockInFragment clockInFragmentObj = new clockInFragment();
         FragmentTransaction defaultTransaction = getSupportFragmentManager().beginTransaction();
-defaultTransaction.replace(R.id.buttonFragment, clockInFragmentObj);
-defaultTransaction.commit();
+        defaultTransaction.replace(R.id.buttonFragment, clockInFragmentObj);
+        defaultTransaction.commit();
 
-
-//        ******* DATABASE FUNCTIONS *******
-
-
-
-//        create object from db helper class
-        DBHelper objDBHelper = new DBHelper(this);
-
-//        call writable method to insert values to database
-        SQLiteDatabase db = objDBHelper.getWritableDatabase();
-
-//        create values object for key value pairs to pass to writable method
-        ContentValues values = new ContentValues();
-
-//      test values to insert into database
-        values.put(objDBHelper.date, dateString);
-        values.put(objDBHelper.clockInTime, "09:00");
-        values.put(objDBHelper.clockOutTime, "17:00");
-
-//        insert to database
-//        long result = db.insert("attendance", null, values);
-//
-////        feedback on status
-//        if(result ==-1) {
-//            Toast.makeText(DashBoard.this, "Values not inserted", Toast.LENGTH_LONG).show();
-//        } else {
-//            Toast.makeText(DashBoard.this, "Values inserted on row" + result, Toast.LENGTH_LONG).show();
-//        }
-//
     }
 
-//    clock toggle method change button text
-    public void toggleClockStatus() {
-        clockedIn = !clockedIn;
-        if (clockedIn) {
-            clockBtn.setText("Clock in");
-        } else {
-            clockBtn.setText("Clock out");
-        }
-    }
 }
