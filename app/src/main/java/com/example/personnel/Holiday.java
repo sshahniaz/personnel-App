7be7981eb4e26591f8d3909856a534e2d98019f8
sshahniaz <100251uk@saeinstitute.edu>
2023-07-22 15:13:39 +0100

    Added backend parts

--- app/src/main/java/com/example/personnel/Holiday.java
+++ app/src/main/java/com/example/personnel/Holiday.java
@@ -2,8 +2,13 @@ package com.example.personnel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Holiday extends AppCompatActivity {
 
@@ -11,6 +16,26 @@ public class Holiday extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_holiday);
+
+        //Display Holidays
+
+
+
+        //Book Holiday
+        DBHelper leaveDB = new DBHelper(this);
+        SQLiteDatabase holidayDB = leaveDB.getWritableDatabase();
+
+        //Get date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        Date now = cal.getTime();
+        String dateNow = dateFormat.format(now);
+
+
+        //Leave status is int so 0 == Pending, 1 == Approved, -1 == Rejected;
+        ContentValues values = new ContentValues();
+
+
 
 
 
