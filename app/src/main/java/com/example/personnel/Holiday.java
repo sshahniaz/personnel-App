d17411105ae7347c15d0b3190ce323cf144f74c4
sshahniaz <100251uk@saeinstitute.edu>
2023-07-23 11:30:12 +0100

    Added date picker

--- app/src/main/java/com/example/personnel/Holiday.java
+++ app/src/main/java/com/example/personnel/Holiday.java
@@ -1,19 +1,25 @@
 package com.example.personnel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.personnel.holidayFiles.HolidayCardDataModel;
 import com.example.personnel.holidayFiles.HolidayListAdapter;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -22,6 +28,11 @@ import java.util.List;
 
 public class Holiday extends AppCompatActivity {
     RecyclerView holidayCardList;
+
+    CardView datePickStartDate;
+    DatePickerDialog datePickerDialog;
+    Date selectedDate;
+    TextView dateStartDay, dateStartMonth, dateStartYear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,21 +63,80 @@ public class Holiday extends AppCompatActivity {
         DBHelper leaveDB = new DBHelper(this);
         SQLiteDatabase db = leaveDB.getWritableDatabase();
 
-        //Get date
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
-        Calendar cal = Calendar.getInstance();
-        Date now = cal.getTime();
-        String dateNow = dateFormat.format(now);
+        //Set Current Date on start
+
+        //start date card set time.
+        dateStartDay = findViewById(R.id.date_start_day);
+        dateStartMonth = findViewById(R.id.date_start_month);
+        dateStartYear = findViewById(R.id.date_start_year);
+
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MMM");
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+        final Calendar cal = Calendar.getInstance();
+        Date dateToday = cal.getTime();
+
+        dateStartDay.setText(dayFormat.format(dateToday));
+        dateStartMonth.setText(monthFormat.format(dateToday));
+        dateStartYear.setText(yearFormat.format(dateToday));
+
+        //Get Datepicker from card on click
+        datePickStartDate = findViewById(R.id.datePick_start_date);
+        datePickStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Get current date for dialog
+                SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+                SimpleDateFormat monthFormat = new SimpleDateFormat("MMM");
+                SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+                final Calendar cal = Calendar.getInstance();
+                int mYear = cal.get(Calendar.YEAR); // current year
+                int mMonth = cal.get(Calendar.MONTH); // current month
+                int mDay = cal.get(Calendar.DAY_OF_MONTH); // current day
+
+//                Date now = cal.getTime();
+
+
+                datePickerDialog = new DatePickerDialog(Holiday.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        SimpleDateFormat req = new SimpleDateFormat("dd-MM-yyyy");
+                        String dateSelected = dayOfMonth+"-"+month+"-"+year;
+
+                        try {
+                            selectedDate = req.parse(dateSelected);
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        String dayNow = dayFormat.format(selectedDate);
+                        String monthNow = monthFormat.format(selectedDate);
+                        String yearNow = yearFormat.format(selectedDate);
+
+
+
+                        dateStartDay.setText(dayNow);
+                        dateStartMonth.setText(monthNow);
+                        dateStartYear.setText(yearNow);
+                    }
+                },mYear,mMonth,mDay);
+                datePickerDialog.show();
+
+            }
+        });
+
+
 
 
         //Leave status is int so 0 == Pending, 1 == Approved, -1 == Rejected;
         ContentValues values = new ContentValues();
-        values.put(leaveDB.employeeId, 1);
-        values.put(leaveDB.startDate,dateNow);
-        values.put(leaveDB.endDate,dateNow);
-        values.put(leaveDB.leaveType,"Sick");
-        values.put(leaveDB.reason,"Due to severe fever and headache");
-        values.put(leaveDB.status,0);
+//        values.put(leaveDB.employeeId, 1);
+//        values.put(leaveDB.startDate,dateNow);
+//        values.put(leaveDB.endDate,dateNow);
+//        values.put(leaveDB.leaveType,"Sick");
+//        values.put(leaveDB.reason,"Due to severe fever and headache");
+//        values.put(leaveDB.status,0);
 
         //Test
 //        long result = db.insert(leaveDB.leaveTable,null,values);
