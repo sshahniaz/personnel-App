4c4494f43f12cb02a8a6f5e24282936e6ee9df40
irinadica <irina_andreea_dica@yahoo.com>
2023-07-26 11:29:25 +0100

    menu files

---
+++ app/src/main/java/com/example/personnel/Menu.java
@@ -0,0 +1,95 @@
+package com.example.personnel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Menu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView=(NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+
+
+
+        if(savedInstanceState==null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home_fragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int item_id=item.getItemId();
+        if(item_id==R.id.nav_home)
+        {
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home_fragment()).commit();
+        }
+        else
+        if(item_id==R.id.nav_holiday)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Holiday_fragment()).commit();
+
+        }
+        else
+        if(item_id==R.id.nav_payslip)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Payslip_fragment()).commit();
+
+        }
+        else
+        if(item_id==R.id.nav_rota)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Rota_fragment()).commit();
+
+        }
+        else
+        if(item_id==R.id.nav_messages)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Messages_fragment()).commit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+}
